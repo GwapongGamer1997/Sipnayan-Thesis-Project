9efe0a01a652135a24c5f3995d2fb06934922fac
Marielle Ranes <pardstonet@gmail.com>
2017-12-06 12:26:44 +0800

    Major Chnges

---
+++ Sipnayan/Assets/Scripts/TabSliderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabSliderScript : MonoBehaviour
+{
+    public void DisableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", false);
+    }
+
+    public void EnableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", true);
+    }
+}
