0a624b8eb60f6612244ba3ff038c75da32cf9442
Marielle Ranes <pardstonet@gmail.com>
2017-12-06 17:00:18 +0800

    add new

---
+++ Sipnayan/Assets/Scripts/FadeInScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInScript : MonoBehaviour
+{
+    public Image splashImage;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+}
---
+++ Sipnayan/Assets/Scripts/games/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	private int num;
+
+	private int countGuess;
+
+	[SerializeField]
+	private GameObject btn;
+
+	[SerializeField]
+	private InputField input;
+
+	[SerializeField]
+	private Text text;
+
+	void Awake() {
+		num = Random.Range (0, 20);
+		text.text = "Guess A Number Between 0 and 20";
+	}
+
+	public void GetInput (string guess) {
+		CompareGuesses (int.Parse(guess));
+		input.text = "";
+		countGuess++;
+	}
+
+	void CompareGuesses(int guess) {
+		if (guess == num) {
+			text.text = "You Guessed Correctly The Number Was " + guess + " It Took You "  + countGuess +  " Guess(ess) Do You Want To Play Again?";
+			btn.SetActive (true);
+		} else if (guess < num) {
+			text.text = "Your Guess Number is Less Than The Number You are Trying to Guess";
+		} else if (guess > num) {
+			text.text = "Your Guess Number is Greater Than The Number You are Trying to Guess";
+		}
+			
+	}
+
+	public void Playback() {
+		num = Random.Range (0, 20);
+		text.text = "Guess A Number Between 0 and 20";
+		countGuess = 0;
+		btn.SetActive (false);
+	}
+}
